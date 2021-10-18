7376e30630e5e37f9ee23f83cd98415016144f55
Emilio Haro <ermsharo@gmail.com>
2021-10-18 19:48:06 -0300

    upload our changes

--- my-app/src/components/fileGet.js
+++ my-app/src/components/fileGet.js
@@ -1,10 +1,35 @@
 
 
-export default function fileGet(){
+
+import React from 'react'
+import {useFetch} from '../services/requests';
+
+export default function UploadForm(props) {
 
 
-     
+    const [response, loading, hasError] = useFetch("http://localhost:5000/data")
+
+if(!response) return (<div>Loading ... </div>)
 
 
-    return (<div>File get here</div>)
+  return(
+    <div>
+
+    <div>Insira aqui seu arquivo csv</div>
+    <br/> <br/>
+{JSON.stringify(response)}
+      <input type="file" name="docx" onChange={setFile.bind(this)} />
+      <input type="button" onClick={postFile} value="Upload" />
+    </div>
+  )
+  function postFile(event) {   
+    // HTTP POST  
+  }
+  function setFile(event) {
+    // Get the details of the files
+    console.log(event.target.files)
+  }
 }
+
+
+
