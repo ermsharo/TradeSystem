8dd732a3f68ae1fb34ec9c4cde65bdf6766e7af0
Emilio Haro <ermsharo@gmail.com>
2021-11-28 22:04:39 -0300

    upload changes

---
+++ build/precache-manifest.41bfe4ae558e4b8aa48d4fdb0c8c8fed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7e3d483f52eebad72537ca7e10ffffd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "596e8ffb9beebd361fc1",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ab2008009aa495e4d9e9",
+    "url": "/static/js/2.46e80fde.chunk.js"
+  },
+  {
+    "revision": "de82c366be47029053e54e4c48092865",
+    "url": "/static/js/2.46e80fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02dc7659a5a775e1fa63",
+    "url": "/static/js/3.985657b2.chunk.js"
+  },
+  {
+    "revision": "596e8ffb9beebd361fc1",
+    "url": "/static/js/main.a613b718.chunk.js"
+  },
+  {
+    "revision": "b05f8efdfdac7c800660",
+    "url": "/static/js/runtime-main.65a93657.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
